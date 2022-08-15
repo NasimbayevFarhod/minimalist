f3c3d056fa55048dfaefa3687e8120257a674199
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-08-15 10:41:47 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var elModalOpenButton = document.querySelector(".nav");
+var elModalOpenlink = document.querySelector(".navbar");
+var elBody = document.querySelector("body")
+
+elModalOpenButton.addEventListener("click", function () {
+  elModalOpenlink.classList.toggle("nav--active");
+});
+
+elModalOpenButton.addEventListener("click", function () {
+  elModalOpenButton.classList.toggle("close");
+});
+
+elModalOpenButton.addEventListener("click", function () {
+  elBody.classList.toggle("overflow");
+});
+
